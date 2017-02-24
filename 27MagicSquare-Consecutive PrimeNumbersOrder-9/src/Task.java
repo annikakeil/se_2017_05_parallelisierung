3de495f76073b15c54336f363ad24ee8cb1c43de
Annika Keil <annika.keil95@gmail.com>
2017-02-24 15:58:53 +0100

    doStrategy veränder Matrix zufällig nach einer Strategie. run macht X Iterationen mit doStrategy Ausfruf

--- 27MagicSquare-Consecutive PrimeNumbersOrder-9/src/Task.java
+++ 27MagicSquare-Consecutive PrimeNumbersOrder-9/src/Task.java
@@ -1,2 +1,34 @@
-public class Task {
+public class Task implements Runnable {
+
+    private Matrix matrix = Matrix.generate(9, 1000);
+
+    private void doStrategy() {
+        MersenneTwisterFast random = new MersenneTwisterFast();
+        float value = random.nextFloat();
+        IStrategy strategy;
+        if (value < 0.5) {
+            strategy = new ShakeMatrix();
+        } else if (value < 0.67) {
+            strategy = new SwapColumn();
+        } else if(value < 0.84) {
+            strategy = new SwapLines();
+        } else {
+            strategy = new SwapDiagonal();
+        }
+
+        matrix = strategy.doStrategy(matrix);
+    }
+
+    @Override
+    public void run() {
+        int iteraton = 0;
+        while (iteraton < Configuration.instance.maxIterationsEachThread && !matrix.isMagic()) {
+            doStrategy();
+            iteraton++;
+        }
+
+        if (matrix.isMagic()) {
+            System.out.println(matrix);
+        }
+    }
 }
